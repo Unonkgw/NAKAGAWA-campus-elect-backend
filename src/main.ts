0df8e61bef81f64f8176c9363c09b95d9e7e2470
Honeydei Yssabelle Baran Nakagawa <hybnakagawa@addunet.ph>
2025-06-09 15:28:48 +0800

    feat: Added Prisma and Tables

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { NestExpressApplication } from '@nestjs/platform-express';     
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT ?? 3001
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  const PORT = process.env.PORT ?? 3001
   await app.listen(PORT);
 
   console.log(`RUNNING ON PORT: ${PORT}`)
